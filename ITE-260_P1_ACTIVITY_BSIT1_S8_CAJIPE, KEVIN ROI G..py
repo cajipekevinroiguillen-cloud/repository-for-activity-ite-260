d4195977d66bb0acbdae4da80d9314811afd8ae3
Cajipe, Kevin Roi Guillen <cajipekevinroiguillen@gmail.com>
2026-08-18 17:21:11 +0800

    Refactor receipt printing and calculations

--- ITE-260_P1_ACTIVITY_BSIT1_S8_CAJIPE, KEVIN ROI G..py
+++ ITE-260_P1_ACTIVITY_BSIT1_S8_CAJIPE, KEVIN ROI G..py
@@ -63,3 +63,46 @@ print(f"{'TOTAL':<28}{total:>10.2f}")
 print("========================================")
 print("       Thank you for shopping!          ")
 print("========================================")
+
+C:\Users\Boss K\PycharmProjects\WelcomeScreen\.venv\Scripts\python.exe" "C:\Users\Boss K\PycharmProjects\WelcomeScreen\ITE-260_P1_ACTIVITY_BSIT1_S8_CAJIPE, KEVIN ROI G..py" 
+========================================
+        RaceCraft Garage Recipe         
+========================================
+Customer Name: CAJIPE KEVIN ROI GUILLEN
+Contact Number: 09648997046
+Address: SNDE
+Product 1
+Product Name: ROLL CAGE
+Price:20
+Quantity: 1
+Product 2
+Product Name: FULL EXHAUST 
+Price: 30
+Quantity: 1
+Product 3
+Product Name: Yokohama Advan A050 TIRE 
+Price: 61
+Quantity: 4
+Discount (%): 30
+========================================
+     RaceCraft Garage Recipe              
+========================================
+Customer Name : CAJIPE KEVIN ROI GUILLEN
+Contact No.   : 09648997046
+Address       : SNDE
+----------------------------------------
+Product           Price  Qty    Amount
+----------------------------------------
+ROLL CAGE         20.00    1     20.00
+FULL EXHAUST      30.00    1     30.00
+Yokohama Advan A050 TIRE    61.00    4    244.00
+----------------------------------------
+Subtotal                        294.00
+Discount                         88.20
+----------------------------------------
+TOTAL                           205.80
+========================================
+       Thank you for shopping!          
+========================================
+
+Process finished with exit code 0
